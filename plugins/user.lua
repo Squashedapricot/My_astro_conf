e10cc8136e0f4574a42da4858cbd751cbdb6f24c
Squashedapricot <shriss999@protonmail.com>
2023-04-04 15:07:12 +0530

    Added cheetsheet and personal touches

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,58 @@
+-- Install and Download Configs Here
+
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
   -- {
-  --   "ray-x/lsp_signature.nvim",
+  --   "ray-x/lsp_signature.nvim", # write config name/github repo_name
   --   event = "BufRead",
   --   config = function()
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "catppuccin/nvim", --# name of the plugin
+    name = "catppuccin",
+    config = function() --# call this config function to write the config for the plugin
+      require("catppuccin").setup {
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        transparent_background = true,
+        show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+        term_colors = true,
+        dim_inactive = {
+          enabled = true,
+          shade = "dark",
+          percentage = 0.15,
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        styles = {
+          comments = { "italic" },
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          telescope = true,
+          notify = false,
+          mini = false,
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+      }
+    end,
+  },
 }
